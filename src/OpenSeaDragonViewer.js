7467e66d832c6180dc17a2631e07f171a488081b
sajald77 <sazzaldulal@gmail.com>
2021-01-12 20:51:35 +0545

    implemented basic annotorious package

--- src/OpenSeaDragonViewer.js
+++ src/OpenSeaDragonViewer.js
@@ -1,4 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import OpenSeaDragon from "openseadragon";
+import Annotorious from "@recogito/annotorious-openseadragon";
 import React, { useEffect, useState } from "react";
 
 
@@ -12,21 +14,47 @@ const OpenSeaDragonViewer = ({ image }) => {
   }, [image]);
 
   const InitOpenseadragon = () => {
+
     viewer && viewer.destroy();
-    setViewer(
-      OpenSeaDragon({
-        id: "openSeaDragon",
-        prefixUrl: "openseadragon-images/",
-        animationTime: 0.5,
-        blendTime: 0.1,
-        constrainDuringPan: true,
-        maxZoomPixelRatio: 2,
-        minZoomLevel: 1,
-        visibilityRatio: 1,
-        zoomPerScroll: 2
-      })
-    );
+
+    const viewerInstance = OpenSeaDragon({
+      id: "openSeaDragon",
+      prefixUrl: "openseadragon-images/",
+      animationTime: 0.5,
+      blendTime: 0.1,
+      constrainDuringPan: true,
+      maxZoomPixelRatio: 2,
+      minZoomLevel: 1,
+      visibilityRatio: 1,
+      zoomPerScroll: 2
+    })
+
+    setViewer(viewerInstance);
+
+    let anno = Annotorious(viewerInstance);
+
+    console.log('checking anno', anno)
+
+    anno.on("createAnnotation", handleCreateAnnotation);
+
+    anno.on("updateAnnotation", handleUpdateAnnotation);
+
+    anno.on("deleteAnnotation", handleDeleteAnnotation);
   };
+
+
+  const handleCreateAnnotation = annotation => {
+    console.log("checking create annotation", annotation)
+  }
+
+  const handleUpdateAnnotation = annotation => {
+    console.log("checking update annotation", annotation)
+  }
+
+  const handleDeleteAnnotation = annotation => {
+    console.log("checking delete annotation", annotation)
+  }
+
 
   useEffect(() => {
     InitOpenseadragon();
